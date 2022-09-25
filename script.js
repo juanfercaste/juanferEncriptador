bc31003f290c0f98806fabe798940b78e5288e40
juanfercaste <djtv122@gmail.com>
2022-09-24 23:53:34 -0600

    nueva version

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 "use strict";
-const mensaje = document.getElementById("input-text");
-const btnEncriptar = document.querySelector(".btn-primario");
-const btnDesencriptar = document.querySelector(".btn-secundario");
+const mensaje = document.getElementById("mensaje");
+const mensaje2 = document.getElementById("mensaje2");
+const btnEncriptar = document.querySelector(".encriptar");
+const btnDesencriptar = document.querySelector(".desencriptar");
 const btnCopiar = document.querySelector(".copiar");
 let encriptado = 0;
 
-const displayMensaje = function (display) {
-  document.querySelector(".mensaje-primero").style.display = display;
-};
-
-const displayMensaje2 = function (display2) {
-  document.querySelector(".mensaje-dos").style.display = display2;
-};
-
 const encriptadoValor = function (valor) {
-  document.querySelector(".encriptado").value = valor;
+  document.getElementById("mensaje2").value = valor;
 };
 
 const encriptar = function () {};
@@ -26,10 +19,8 @@ btnEncriptar.addEventListener("click", function () {
     .replaceAll("o", "ober")
     .replaceAll("a", "ai")
     .replaceAll("u", "ufat");
-  displayMensaje("none");
-  displayMensaje2("block");
   encriptadoValor(encriptado);
-  document.querySelector(".input-text").value = "";
+  mensaje.value = "";
 });
 
 btnDesencriptar.addEventListener("click", function () {
@@ -39,14 +30,12 @@ btnDesencriptar.addEventListener("click", function () {
     .replaceAll("ober", "o")
     .replaceAll("ai", "a")
     .replaceAll("ufat", "u");
-  displayMensaje("none");
-  displayMensaje2("block");
   encriptadoValor(encriptado);
+  mensaje.value = "";
 });
 
 btnCopiar.addEventListener("click", function () {
   navigator.clipboard.writeText(encriptado);
-  displayMensaje("block");
-  displayMensaje2("none");
-  document.querySelector(".input-text").value = "";
+  mensaje.value = encriptado;
+  mensaje2.value = "";
 });
